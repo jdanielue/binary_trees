9650798503b138870591efcde47bb6bafb17463a
jdanielue <jdurregoe@gmail.com>
2021-03-10 16:27:34 -0500

    s6a0

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,1 +1,19 @@
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * @func: pointer to a function to call for each node
+ * Return: 1 if is root
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	func(tree->n);
+	if (tree->left == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	if (tree->right == NULL)
+		return;
+	binary_tree_preorder(tree->right, func);
+}
