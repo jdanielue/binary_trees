3b8e7b199491ed555d95bf6273828bb407f241ca
jdanielue <jdurregoe@gmail.com>
2021-03-10 17:23:40 -0500

    s8a3

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,23 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree
+ * binary_tree_postorder - function that goes through a binary tree
  * @tree: pointer to the node to check
  * @func: pointer to a function to call for each node
  * Return: 1 if is root
  */
 
 
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (func != NULL && tree != NULL)
 	{
-		if (tree->left == NULL)
-			return;
-		binary_tree_postorder(tree->left, func);
-		if (tree->right == NULL)
-			return;
-		binary_tree_postorder(tree->right, func);
+		if (tree->left != NULL)
+            binary_tree_postorder(tree->left, func);
+		if (tree->right != NULL)
+		    binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
 }
