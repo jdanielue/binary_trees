c2bf7d355ea57e56fe270be7121824175853b149
jdanielue <jdurregoe@gmail.com>
2021-03-10 17:24:57 -0500

    s8a4

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,9 +13,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (func != NULL && tree != NULL)
 	{
 		if (tree->left != NULL)
-            binary_tree_postorder(tree->left, func);
+			binary_tree_postorder(tree->left, func);
 		if (tree->right != NULL)
-		    binary_tree_postorder(tree->right, func);
+			binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
 }
