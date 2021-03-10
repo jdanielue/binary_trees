450720e744c708802939547f80f282629033a897
jdanielue <jdurregoe@gmail.com>
2021-03-10 02:43:31 -0500

    s3a0

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,18 @@
-void binary_tree_delete(binary_tree_t *tree);
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - inserts a node as the left-child of another node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: A pointer
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+    free(tree);
+
+}
