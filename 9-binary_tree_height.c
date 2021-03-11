76c1629d5c104e359307fd060198a3cf046effc5
jdanielue <jdurregoe@gmail.com>
2021-03-11 15:56:49 -0500

    s9a0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,17 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t counter_l = 0, counter_r = 0;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
 	if (tree->left)
-	{
 		(counter_l = 1 + binary_tree_height(tree->left));
-	}
 	if (tree->right)
-	{
 		(counter_r = 1 + binary_tree_height(tree->right));
-	}
 	if (counter_l >= counter_r)
 		return (counter_l);
 	else
