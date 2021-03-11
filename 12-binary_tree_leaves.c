6e9d1232c363d1dce4e85c0a347155d332f644c7
jdanielue <jdurregoe@gmail.com>
2021-03-11 18:05:35 -0500

    s12a0

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,1 +1,40 @@
-size_t binary_tree_leaves(const binary_tree_t *tree);
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_leaves_r - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * @counter: counter
+ * Return: nothing
+ */
+
+
+void binary_tree_leaves_r(const binary_tree_t *tree, size_t *counter)
+{
+	if (tree->left)
+		binary_tree_leaves_r(tree->left, counter);
+
+	if (tree->right)
+		binary_tree_leaves_r(tree->right, counter);
+
+	if (tree->left == NULL && tree->right == NULL)
+		*counter += 1;
+}
+
+
+/**
+ * binary_tree_leaves_ - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * Return: leaf
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	binary_tree_leaves_r(tree, &counter);
+	return (counter);
+}
