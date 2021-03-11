b2b4465be277ae0445264427a7c0de0b7074c4a6
jdanielue <jdurregoe@gmail.com>
2021-03-11 17:01:10 -0500

    s11a0

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,31 +1,40 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_siz_r - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * @counter: counter
+ * Return: nothing
+ */
+
+void binary_tree_size_r(const binary_tree_t *tree, size_t *counter)
+{
+	if (tree->left)
+	{
+		binary_tree_size_r(tree->left, counter);
+		*counter += 1;
+	}
+	if (tree->right)
+	{
+		binary_tree_size_r(tree->right, counter);
+		*counter += 1;
+	}
+}
+
+/**
  * binary_tree_size - function that goes through a binary tree
  * @tree: pointer to the node to check
  * Return: counter
  */
 
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-size_t counter_l = 0, counter_r = 0;
+	size_t counter = 1;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->parent == NULL)
-	{
-		counter_r = binary_tree_size(tree->right);
-		counter_l = binary_tree_size(tree->left);
-		counter_l += 1;
-	}
-
-	if (tree->left && tree->parent != NULL)
-		(counter_l = 1 + binary_tree_size(tree->left));
-	if (tree->right && tree->parent != NULL)
-		(counter_r = 1 + binary_tree_size(tree->right));
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	return (counter_l + counter_r);
+	binary_tree_size_r(tree, &counter);
+	return (counter);
 }
