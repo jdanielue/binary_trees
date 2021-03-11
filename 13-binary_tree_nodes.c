13d7bb6b742f471436b8de683f138d8edce6660b
jdanielue <jdurregoe@gmail.com>
2021-03-11 18:25:23 -0500

    s13a0

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,1 +1,36 @@
-size_t binary_tree_nodes(const binary_tree_t *tree);
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes_r- function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * @counter: counter
+ * Return: counter
+ */
+
+void binary_tree_nodes_r(const binary_tree_t *tree, size_t *counter)
+{
+	if (tree->left)
+		binary_tree_nodes_r(tree->left, counter);
+
+	if (tree->right)
+		binary_tree_nodes_r(tree->right, counter);
+
+	if (tree->left != NULL || tree->right != NULL)
+		*counter += 1;
+}
+/**
+ * binary_tree_nodes - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * Return: counter
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	binary_tree_nodes_r(tree, &counter);
+	return (counter);
+}
