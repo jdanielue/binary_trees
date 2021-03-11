66450f2412ef20ffe79b02ecba545bd604959981
jdanielue <jdurregoe@gmail.com>
2021-03-11 15:57:13 -0500

    s10a0

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,32 @@
-size_t binary_tree_depth(const binary_tree_t *tree);
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth_r - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * @counter: counter
+ * Return: counter
+ */
+
+void binary_tree_depth_r(const binary_tree_t *tree, size_t *counter)
+{
+	if (tree->parent)
+	{
+		binary_tree_depth_r(tree->parent, counter);
+		*counter += 1;
+	}
+}
+
+/**
+ * binary_tree_depth - function that goes through a binary tree
+ * @tree: pointer to the node to check
+ * Return: counter
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	binary_tree_depth_r(tree, &counter);
+
+	return (counter);
+}
